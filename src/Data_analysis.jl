98901ff9b8c9dcd0f47b612fcdadd52bb2fd14bd
Simon Delcamp <simon.delcamp@univ-grenoble-alpes.fr>
2023-06-01 12:29:49 +0200

    Pb with metric

--- src/Data_analysis.jl
+++ src/Data_analysis.jl
@@ -62,7 +62,7 @@ Calculate the following metric on each values of mom1,2,3 and 4: sqrt((mom1/dv)*
 function calcmetricOW(mom1,mom2,mom3,mom4,dv,SIGMAT)
     metric = similar(mom1)
     for ix=1:size(metric)[1]
-        metric[ix] = sqrt((mom1[ix]/dv)^2 + ((mom2[ix]/dv))^2+(mom3[ix]-SIGMAT)^2+(mom4[ix]-3)^2)
+        metric[ix] = sqrt((mom1[ix]/dv)^2 + ((mom2[ix]-SIGMAT)/dv)^2+(mom3[ix])^2+(mom4[ix]-3)^2)
     end
     return(metric)
 
